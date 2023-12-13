6bdb212a9c75bfa04b2cceb2b1447152dec46a05
Edward <hua9939@gmail.com>
2023-12-13 21:45:47 +1300

    add the text content to pdf

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 # Go through each txt file
 for filepath in filepaths:
-    # Read the data from the file
-    data = open(filepath, "r")
-
     # Get the file name
     filename = Path(filepath).stem
     name = filename.title()
@@ -20,9 +17,18 @@ for filepath in filepaths:
     # Add a page to the PDF file
     pdf.add_page()
 
-    # Write the name to the PDF file
+    # Write the page name to the PDF file
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.cell(w=0, h=12, txt=name)
+    pdf.cell(w=0, h=12, txt=name, ln=1)
+
+    # Read the data from the file
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    # Write the content to the page
+    pdf.set_font(family="Times", style="", size=12)
+    pdf.multi_cell(w=0, h=5, txt=content, border=0)
+
 
 # Generate the PDF file
 pdf.output("PDFs/output.pdf")
